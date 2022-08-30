629f76958d603a13235841936c3c07f4acfe412d
shalesh1006 <shaleshdutt1006@gmail.com>
2022-08-30 09:07:19 +0530

    Creating : Creating the master branch

---
+++ src/PrintElements.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintElements {
+
+    public static void main(String[] args) {
+
+
+   int[] array = {10,20,40,80,90,500,600};
+
+        for (int elements:array) {
+
+            System.out.println(elements);
+
+        }
+
+
+
+    }
+}
